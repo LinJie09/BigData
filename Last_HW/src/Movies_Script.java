9b7b71cbcec47be403eee5211bc197ccf93616d2
LinJie09 <zxc664231@gmail.com>
2021-12-21 15:39:25 +0800

    Final

--- Last_HW/src/Movies_Script.java
+++ Last_HW/src/Movies_Script.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Movies_Script {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Movies_Script {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://gimy.app/").get();
+            System.out.println(doc.title());
+            Elements authorClasses = doc.select(".container .col-lg-6  col-md-6 col-sm-4 col-xs-3");
+            System.out.println(authorClasses);
+/*            for (Element authorClass : authorClasses) {
+                String Topic = authorClasses.select(".sc-15-yfh73-4.jPnNqj").text();
+                Document hotBoardDoc = Jsoup.connect().get();
+                String articleSortDoc = authorClass.absUrl("href");
+                System.out.println("author: "+ articleSortDoc);
+                System.out.println("Topic: "+ Topic);
+            }*/
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
