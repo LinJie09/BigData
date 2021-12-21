6deed960d1b8cc1b77f50737977333fe1e9186fd
LinJie09 <zxc664231@gmail.com>
2021-12-21 15:34:16 +0800

    Final

--- Last_HW/src/Spider_Food.java
+++ Last_HW/src/Spider_Food.java
@@ -6,15 +6,17 @@ import org.jsoup.select.Elements;
 public class Spider_Food {
     public static void main(String[] args) {
         try {
-            Document doc = Jsoup.connect("https://www.pixnet.net/tags/%E9%AB%98%E9%9B%84%E7%BE%8E%E9%A3%9F?utm_source=PIXNET&utm_medium=navbar&utm_term=search_result_tag&utm_content=%E9%AB%98%E9%9B%84%E7%BE%8E%E9%A3%9F").get();
+            Document doc = Jsoup.connect("https://gimy.app/").get();
             System.out.println(doc.title());
-            Elements authorClasses =doc.select("#root .sc-15yfh73-3.dTOYFB .sc-15-yfh73-4.jPnNqj a.sc-15yfh73-5.clZsAM");
-            for (Element authorClass : authorClasses) {
+            Elements authorClasses = doc.select(".container .col-lg-6  col-md-6 col-sm-4 col-xs-3");
+            System.out.println(authorClasses);
+/*            for (Element authorClass : authorClasses) {
                 String Topic = authorClasses.select(".sc-15-yfh73-4.jPnNqj").text();
+                Document hotBoardDoc = Jsoup.connect().get();
                 String articleSortDoc = authorClass.absUrl("href");
                 System.out.println("author: "+ articleSortDoc);
                 System.out.println("Topic: "+ Topic);
-            }
+            }*/
         }catch (Exception e){
             System.out.println(e);
         }
