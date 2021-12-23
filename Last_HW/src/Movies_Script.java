15e1663320ea083df89196a06690e489f1bb31d8
LinJie09 <zxc664231@gmail.com>
2021-12-23 17:20:55 +0800

    Final

--- Last_HW/src/Movies_Script.java
+++ Last_HW/src/Movies_Script.java
@@ -12,6 +12,7 @@ public class Movies_Script {
 //            System.out.println(moviesTitle);
             for (Element moviesClass : moviesTitle) {
                 Elements titleName = doc.select("li .myui-vodlist__detail");
+                //16行是抓 a Tag 裡的 Title 
                 System.out.println("Title : "+ titleName.select("a").attr("title"));
                 String titleDoc = moviesClass.absUrl("href");
                 Document moviesDoc = Jsoup.connect(titleDoc).get();
