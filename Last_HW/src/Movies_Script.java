d88fee0f969d48a816b452a0078059a84a7ae131
LinJie09 <zxc664231@gmail.com>
2021-12-23 14:53:21 +0800

    finalHw

--- Last_HW/src/Movies_Script.java
+++ Last_HW/src/Movies_Script.java
@@ -1,21 +1,23 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Movies_Script {
     public static void main(String[] args) {
         try {
-            Document doc = Jsoup.connect("https://gimy.app/").get();
+            Document doc = Jsoup.connect("https://gimytv.com/browse/movies.html").get();
             System.out.println(doc.title());
-            Elements authorClasses = doc.select(".container .col-lg-6  col-md-6 col-sm-4 col-xs-3");
-            System.out.println(authorClasses);
-/*            for (Element authorClass : authorClasses) {
-                String Topic = authorClasses.select(".sc-15-yfh73-4.jPnNqj").text();
-                Document hotBoardDoc = Jsoup.connect().get();
-                String articleSortDoc = authorClass.absUrl("href");
-                System.out.println("author: "+ articleSortDoc);
-                System.out.println("Topic: "+ Topic);
-            }*/
+            Elements moviesTitle = doc.select(".container .row li div.myui-vodlist__box h4.title.text-overflow a");
+            System.out.println(moviesTitle);
+            for (Element moviesClass : moviesTitle) {
+                Elements TitleElement = doc.select("div.myui-vodlist__detail h4.title.text-overflow a");
+                String titleName = TitleElement.attr("title");
+//                System.out.println(titleName);
+                String titleDoc = moviesClass.absUrl("href");
+                Document moviesDoc = Jsoup.connect(titleDoc).get();
+                System.out.println("Movies Title Link: "+ titleDoc);
+            }
         }catch (Exception e){
             System.out.println(e);
         }
