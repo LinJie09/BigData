5f24aced33c78baa180b779e5fe7f95bc59e0a7d
LinJie09 <zxc664231@gmail.com>
2021-12-23 17:17:23 +0800

    finalHw

--- Last_HW/src/Movies_Script.java
+++ Last_HW/src/Movies_Script.java
@@ -9,11 +9,10 @@ public class Movies_Script {
             Document doc = Jsoup.connect("https://gimytv.com/browse/movies.html").get();
             System.out.println(doc.title());
             Elements moviesTitle = doc.select(".container .row li div.myui-vodlist__box h4.title.text-overflow a");
-            System.out.println(moviesTitle);
+//            System.out.println(moviesTitle);
             for (Element moviesClass : moviesTitle) {
-                Elements TitleElement = doc.select("div.myui-vodlist__detail h4.title.text-overflow a");
-                String titleName = TitleElement.attr("title");
-//                System.out.println(titleName);
+                Elements titleName = doc.select("li .myui-vodlist__detail");
+                System.out.println("Title : "+ titleName.select("a").attr("title"));
                 String titleDoc = moviesClass.absUrl("href");
                 Document moviesDoc = Jsoup.connect(titleDoc).get();
                 System.out.println("Movies Title Link: "+ titleDoc);
